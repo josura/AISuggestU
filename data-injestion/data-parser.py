beefb084778bef8a086241251cd9ab525c18850a
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-07 23:41:41 +0200

    Update data injestion

--- data-injestion/data-parser.py
+++ data-injestion/data-parser.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from github import Github
+from collections import defaultdict
 
 def cleanLink(value):
     value = value[1:]
@@ -16,8 +17,14 @@ def cleanLink(value):
     return value[:i]
 
 def filterData(data):
-    url, owner = data['url'], data['owner']
-    return json.dumps({"url":url, "owner": owner})
+    dataDict = data.loads()
+    
+    # regroup data
+    d = defaultdict(dict)
+    for item in dataDict:
+        d[item["id"]].update(item)
+
+
 
 f = open("data.json", "w")
 link = "https://api.github.com/repositories"
@@ -25,7 +32,7 @@ link = "https://api.github.com/repositories"
 for i in range(0, 3):
     data = requests.get(link)
     header = data.headers
-    data = filterData(data.json)
+    filterData(data.json)
     f.write(data.text.encode('utf-8'))
     link = cleanLink(header['link'])
     #Get url, repo_id, owner
