3dee9d6a541afbe082a20a3c9f6bf3fa49cc2089
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-07 23:17:56 +0200

    Add data parser

---
+++ data-injestion/data-parser.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import os
+from github import Github
+
+def cleanLink(value):
+    value = value[1:]
+
+    i = 0
+
+    for el in value:
+        if el == '>':
+            break
+        i += 1
+    
+    return value[:i]
+
+def filterData(data):
+    url, owner = data['url'], data['owner']
+    return json.dumps({"url":url, "owner": owner})
+
+f = open("data.json", "w")
+link = "https://api.github.com/repositories"
+
+for i in range(0, 3):
+    data = requests.get(link)
+    header = data.headers
+    data = filterData(data.json)
+    f.write(data.text.encode('utf-8'))
+    link = cleanLink(header['link'])
+    #Get url, repo_id, owner
+
+# # or using an access token
+# g = Github(os.environ['TOKEN_GITHUB'])
+
+# for repo in g.get_user().get_repos():
+#     print(repo.name)
+#     repo.edit(has_wiki=False)
+#     print(dir(repo))
