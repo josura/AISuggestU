523ff32c59a448318dff4b7055c87a56c1de06e1
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-10 21:06:12 +0200

    Elastic search first part (this is working)

--- publish-to-elastic/publish-to-elastic.py
+++ publish-to-elastic/publish-to-elastic.py
@@ -12,18 +12,8 @@ if sys.argv > 1:
         hosts=[{'host': '127.0.0.1', 'port': 9200}]
     )
     
-    es = Elasticsearch(ES_CLUSTER)
     with open(sys.argv[1]) as json_file:
         repos = json.load(json_file)
 
         for repo in repos:
-            es.bulk(ES_INDEX, ES_TYPE, )
-
-    # e1={
-    # "first_name" :  "asd",
-    # "last_name" :   "asdasd",
-    # "age" :         33,
-    # "about" :       "I like to collect rock albums",
-    # "interests":  [ "music" ]
-    # }
-    # res = es.index(index='pippo2',id=100,body=e1)
+            res = es.index(index='repository', id=repo['url'], body=repo)
