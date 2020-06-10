2baba13e61bc0c5bee65ab4205f7d5b272d5ece3
josura <giorgiolocicero@hotmail.com>
2020-06-10 14:36:48 +0200

    finished model creation for clustering

--- spark/MLPipeline_clustering_e_classificazione/src/main/scala/Clustering_Classify.scala
+++ spark/MLPipeline_clustering_e_classificazione/src/main/scala/Clustering_Classify.scala
@@ -6,8 +6,17 @@ import org.apache.spark.ml.clustering.LDA
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.Dataset
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
 
 object Clustering_Classify {
+
+  def createPipelineKmeans(numcluster:Int){
+    val tokenizer = new Tokenizer().setInputCol("readme").setOutputCol("words")
+    val hashingTF = new HashingTF().setNumFeatures(1000).setInputCol(tokenizer.getOutputCol).setOutputCol("features")
+    val kmeans = new KMeans().setK(numcluster).setSeed(1L)
+    new Pipeline().setStages(Array(tokenizer, hashingTF, kmeans))
+  }
   def main(args: Array[String]) {
     val spark = SparkSession
         .builder
@@ -15,7 +24,12 @@ object Clustering_Classify {
         .master("local")
         .getOrCreate()
     //TODO Data ingestion e streaming delle repository
-
+    // keep attention on the types returned
+    var readme: Dataset[Row]= spark.read.json("/home/josura/Desktop/AISuggestU/spark/MLPipeline_clustering_e_classificazione/fulldata.json")
+    //readme = readme.dropDuplicates("owner")
+    readme= readme.na.drop
+    readme = readme.filter(length(readme("readme"))>3)
+    val readmeDS=readme.as[RepoTyped]
     var numcluster :Int = 10
     var numiter:Int=10
     val lda = new LDA().setK(numcluster).setMaxIter(numiter)
@@ -23,18 +37,10 @@ object Clustering_Classify {
     // Prepare training repos from a list of (id, text-readme, label) tuples.
     //labels are deducted from previous clustering
 
-    val tokenizer = new Tokenizer()
-        .setInputCol("text")
-        .setOutputCol("words")
-    val hashingTF = new HashingTF()
-        .setNumFeatures(1000)
-        .setInputCol(tokenizer.getOutputCol)
-        .setOutputCol("features")
-
     //TODO Decision of the model, probably based on the decisions of layer before in the pipeline
-    val pipeline = new Pipeline()
-        .setStages(Array(tokenizer, hashingTF, lda))
+    val pipeline = createPipelineKmeans(10)
 
-    val model = pipeline.fit()//dati
+    val model = pipeline fit readme
+    val predictions = model transform readme
   }
 }
