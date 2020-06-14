8cb2a2f54ff016c40736cc0bf8a51a934d70bf10
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-14 14:26:27 +0200

    Clean code publish-to-elastic.py

--- publish-to-elastic/publish-to-elastic.py
+++ publish-to-elastic/publish-to-elastic.py
@@ -4,12 +4,8 @@ import os, sys
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 
 if sys.argv > 1:
-    ES_CLUSTER = 'http://127.0.0.1:9200/' # Need PW and User name
-    ES_INDEX = 'daily-data'
-    ES_TYPE = 'json'
-
     es = Elasticsearch(
-        hosts=[{'host': '127.0.0.1', 'port': 9200}]
+        hosts=[{'host': 'elastic-search', 'port': 9200}]
     )
     
     with open(sys.argv[1]) as json_file:
