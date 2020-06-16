1561d23363f726173ddd8081ca4375439caeef3f
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-16 11:30:18 +0200

    Fix some issues

--- starredRepoAndPublish/starredRepo.go
+++ starredRepoAndPublish/starredRepo.go
@@ -10,9 +10,10 @@ import (
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
+//Message rappresents the message to be sent to Kafka
 type Message struct {
-	User     string `json:"user"`
-	Url_Repo string `json:"url_repo"`
+	User    string `json:"user"`
+	URLRepo string `json:"url_repo"`
 }
 
 func userStarred(p *kafka.Producer, val []byte) {
@@ -40,26 +41,12 @@ func main() {
 
 	defer p.Close()
 
-	// Delivery report handler for produced messages
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-				}
-			}
-		}
-	}()
-
 	url := fmt.Sprintf("https://api.github.com/users/%s/starred?per_page=100", argsWithoutProg[0])
 
 	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Println("ERROR")
+		panic(err)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
